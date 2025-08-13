1afc7e034bf611cd9930cdc4764af6992fddbbf7
Macie Medes <mmedes@sandiego.edu>
2025-08-13 15:41:12 -0700

    started speeding up code, takes 11 min 17 sec rn

--- thermodynamics.py
+++ thermodynamics.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import numba as nb
 x,w=np.polynomial.laguerre.laggauss(50)
 
+@nb.njit()
 def I1(u):                                
     g=np.zeros(50)
     for j in range(len(g)):
@@ -9,7 +11,7 @@ def I1(u):
         g[j]=x[j]**2*E*(1/(np.exp(E)+1))
     return np.sum(w*g*np.exp(x))
 
-
+@nb.njit()
 def I2(u):
     g=np.zeros(50)
     for j in range(len(g)):
@@ -17,7 +19,7 @@ def I2(u):
         g[j]=(x[j]**4)/E*(1/(np.exp(E)+1))
     return np.sum(w*g*np.exp(x))
 
-
+@nb.njit()
 def I3(u):
     g=np.zeros(50)
     for j in range(len(g)):
@@ -25,7 +27,7 @@ def I3(u):
         g[j]=x[j]**2*(E**2)*(np.exp(E))/(((np.exp(E)+1))**2)
     return np.sum(w*g*np.exp(x))
 
-
+@nb.njit()
 def I4(u):
     g=np.zeros(50)
     for j in range(len(g)):
@@ -33,29 +35,30 @@ def I4(u):
         g[j]=x[j]**4*(np.exp(E))/(np.exp(E)+1)**2
     return np.sum(w*g*np.exp(x))
 
-
+@nb.njit()
 def p(T,m,g):
     u=m/T
     return ((g*T**4)*(I1(u)))/(2*np.pi**2)
 
-
+@nb.njit()
 def P(T,m,g):
     u=m/T
     return (g*T**4)/(6*np.pi**2)*I2(u)
 
+@nb.njit()
 def dpdT(T,m,g):
     u=m/T
     return (g*T**3*I3(u))/(2*np.pi**2)
 
-
+@nb.njit()
 def dPdT(T,m,g):
     u=m/T
     return (g*T**3*I4(u))/(6*np.pi**2)
 
-
+@nb.njit()
 def s(T,m,g):
     return (1/T)*(p(T,m,g)+P(T,m,g))
 
-
+@nb.njit()
 def dsdT(T,m,g):
     return (1/T)*(dpdT(T,m,g)+dPdT(T,m,g))-(1/T**2)*(p(T,m,g)+P(T,m,g))
